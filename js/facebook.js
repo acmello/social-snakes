477233227f85d5eeacb98393b96763ccb224f388
crisstanza <crisstanza@home>
2012-09-02 08:16:26 -0300

    Facebook API integration: login, permissions and get userId (Sep-02-2012)

---
+++ js/facebook.js
@@ -0,0 +1,55 @@
+$(document).ready(
+  function() {
+    facebookInit();
+  }
+);
+
+function facebookInit() {
+  FB.init({
+    appId: '476027365749233',
+    cookie: true,
+  });
+
+  FB.getLoginStatus(
+    function(response) {
+      if (response.status === 'connected') {
+        // the user is logged in and has authenticated your
+        // app, and response.authResponse supplies
+        // the user's ID, a valid access token, a signed
+        // request, and the time the access token 
+        // and signed request each expire
+        var uid = response.authResponse.userID;
+        // var accessToken = response.authResponse.accessToken;
+        showUser(uid);
+      } else if (response.status === 'not_authorized') {
+        // the user is logged in to Facebook, 
+        // but has not authenticated your app
+          authUser();
+      } else {
+        // the user isn't logged in to Facebook.
+          authUser();
+      }
+    }
+   );
+}
+
+function authUser() {
+  FB.login(
+    function(response) {
+      var uid;
+      if (response.authResponse == null) {
+        uid = 'auth pending';
+      } else {
+        uid = response.authResponse.userID ? response.authResponse.userID : null;
+      }
+      showUser(uid);
+    }, {
+      scope: 'read_friendlists'
+    }
+  );
+}
+
+function showUser(uid) {
+  var user = $('#current_user');
+  user.html('Id: '+uid);
+}
