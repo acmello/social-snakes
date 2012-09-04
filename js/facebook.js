d9b77e7b99b63c07c4a3ea6b1036aea4bca6986d
crisstanza <crisstanza@home>
2012-09-04 02:28:18 -0300

    Global Score Board API integration (Sep-04-2012)

--- js/facebook.js
+++ js/facebook.js
@@ -5,32 +5,34 @@ $(document).ready(
 );
 
 function facebookInit() {
-  FB.init({
-    appId: '476027365749233',
-    cookie: true,
-  });
+  if(typeof(FB) != 'undefined') {
+    FB.init({
+      appId: '476027365749233',
+      cookie: true,
+    });
 
-  FB.getLoginStatus(
-    function(response) {
-      if (response.status === 'connected') {
-        // the user is logged in and has authenticated your
-        // app, and response.authResponse supplies
-        // the user's ID, a valid access token, a signed
-        // request, and the time the access token 
-        // and signed request each expire
-        var uid = response.authResponse.userID;
-        // var accessToken = response.authResponse.accessToken;
-        showUser(uid);
-      } else if (response.status === 'not_authorized') {
-        // the user is logged in to Facebook, 
-        // but has not authenticated your app
-          authUser();
-      } else {
-        // the user isn't logged in to Facebook.
-          authUser();
+    FB.getLoginStatus(
+      function(response) {
+        if (response.status === 'connected') {
+          // the user is logged in and has authenticated your
+          // app, and response.authResponse supplies
+          // the user's ID, a valid access token, a signed
+          // request, and the time the access token 
+          // and signed request each expire
+          var uid = response.authResponse.userID;
+          // var accessToken = response.authResponse.accessToken;
+          showUser(uid);
+        } else if (response.status === 'not_authorized') {
+          // the user is logged in to Facebook, 
+          // but has not authenticated your app
+            authUser();
+        } else {
+          // the user isn't logged in to Facebook.
+            authUser();
+        }
       }
-    }
-   );
+     );
+  }
 }
 
 function authUser() {
