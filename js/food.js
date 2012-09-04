d459ddf9f4ea7a3956807c78ba9c6bc1976e48aa
acmello <acmello.ti@gmail.com>
2012-09-04 18:18:36 -0300

    Update js/food.js

--- js/food.js
+++ js/food.js
@@ -1,40 +1,39 @@
-function Food(game){
-	this.x = 0;
-	this.y = 0;
-	
-	var food = this;
-	
-	this.generateRandomPosition = function(){
-		var x = Math.floor(Math.random()*(game.canvas.width/game.gridSize))*game.gridSize;
-		var y = Math.floor(Math.random()*(game.canvas.width/game.gridSize))*game.gridSize;
+function Food(game) {
+  this.x = 0;
+  this.y = 0;
 
-		var positionArray = [x,y];
+  var food = this;
 
-        console.log("x"+x+" y"+y );
+  this.generateRandomPosition = function() {
+    var x = Math.floor(Math.random() * (game.canvas.width / game.gridSize)) * game.gridSize;
+    var y = Math.floor(Math.random() * (game.canvas.width / game.gridSize)) * game.gridSize;
 
-        food.x = x;
-        food.y = y;
-        
-        // trying to create the element but the snake is already there
-		//if( game.snakeBody != undefined && game.snakeBody.some( food.isSnakeHere ) )
-        //    food.generateRandomPosition();
+    var positionArray = [x, y];
 
-		food.draw();
-	};
+    console.log("x" + x + " y" + y);
 
-    this.isSnakeHere = function(element, index, array) {
-        return ( element[0] == food.x && element[0] == food.y );
-    };
-	
-	this.draw = function(){
-		var ctx = game.ctx;
-		ctx.fillStyle = 'rgb(200,0,0)';
-		ctx.fillRect(food.x, food.y, game.gridSize, game.gridSize);
-	};
+    food.x = x;
+    food.y = y;
 
-  	// clear element on the screen
-   this.clear = function(){
-      var ctx = game.ctx;
-      ctx.clearRect( food.x, food.y, game.gridSize, game.gridSize );
+    // trying to create the element but the snake is already there
+    //if( game.snakeBody != undefined && game.snakeBody.some( food.isSnakeHere ) )
+    //    food.generateRandomPosition();
+    food.draw();
+  };
+
+  this.isSnakeHere = function(element, index, array) {
+    return (element[0] == food.x && element[0] == food.y);
+  };
+
+  this.draw = function() {
+    var ctx = game.ctx;
+    ctx.fillStyle = 'rgb(200,0,0)';
+    ctx.fillRect(food.x, food.y, game.gridSize, game.gridSize);
+  };
+
+  // clear element on the screen
+  this.clear = function() {
+    var ctx = game.ctx;
+    ctx.clearRect(food.x, food.y, game.gridSize, game.gridSize);
   };
 }
