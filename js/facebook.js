e1621172f0c3800c1dbac7b128665bcba3171d9c
crisstanza <crisstanza@home>
2012-09-04 23:03:44 -0300

    Facebook more info

--- js/facebook.js
+++ js/facebook.js
@@ -4,6 +4,11 @@ $(document).ready(
   }
 );
 
+function FBCurrentUser {
+}
+FBCurrentUser.name = null;
+FBCurrentUser.fbId = -1;
+
 function facebookInit() {
   if(typeof(FB) != 'undefined') {
     FB.init({
@@ -54,4 +59,10 @@ function authUser() {
 function showUser(uid) {
   var user = $('#current_user');
   user.html('Id: '+uid);
+  //
+  FB.api('/me', function(response) {
+    FBCurrentUser.name = response.name+" "+response.first_name;
+    FBCurrentUser.fbId = response.id;
+    var imgLink = "http://graph.facebook.com/"+response.id+"/picture"
+  });
 }
